d416152cd161ab14015feff18548ff58d7a4a88b
0naha0 <namhoailu@gmail.com>
2021-10-28 10:32:19 +0700

    init new project

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Client.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class Client extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+
+        btn = (Button)findViewById(R.id.back_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Client.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
