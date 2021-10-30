d6b8bb92a3009fc7d4f05ea4fff206d0e2f0c56c
0naha0 <namhoailu@gmail.com>
2021-10-30 22:21:58 +0700

    old version

--- MyApplication/app/src/main/java/com/example/myapplication/Dish_info.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Dish_info.java
@@ -2,16 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
 
-public class Client extends AppCompatActivity {
+public class Dish_info extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client);
+        setContentView(R.layout.activity_dish_info);
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Client.class);
+                Intent i = new Intent(MainActivity.this, Order_info_1.class);
                 startActivity(i);
             }
         });
