2317ba3e796fe3aa88663bbbfe5704f162d0c7bc
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2021-11-01 21:50:03 +0700

    new version project
    * new version
    
    * Reset new project
    
    Co-authored-by: 0naha0 <namhoailu@gmail.com>

---
+++ AndroidApp/app/src/main/java/com/example/androidapp/OrderInfo1Activity.java
@@ -0,0 +1,14 @@
+package com.example.androidapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderInfo1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_info1);
+    }
+}
