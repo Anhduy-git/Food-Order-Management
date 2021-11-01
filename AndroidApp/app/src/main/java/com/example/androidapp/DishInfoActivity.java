5e3037b10b4ea6070b9ff7e9c0a20ebd0361f9ab
0naha0 <namhoailu@gmail.com>
2021-11-01 21:16:46 +0700

    new version

---
+++ AndroidApp/app/src/main/java/com/example/androidapp/DishInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DishInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish_info);
+    }
+}
