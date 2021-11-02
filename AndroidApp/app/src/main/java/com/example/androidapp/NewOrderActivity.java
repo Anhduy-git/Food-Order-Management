eff87877cf2a45c975e30e382ac33d1158953aca
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2021-11-02 19:44:32 +0700

    Update Order Activity

--- AndroidApp/app/src/main/java/com/example/androidapp/NewOrderActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/NewOrderActivity.java
@@ -2,13 +2,26 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class NewOrderActivity extends AppCompatActivity {
-
+    private Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_order);
+
+        //Button back to OrderTodayFragment
+        btnBack = (Button)findViewById(R.id.new_order_back_btn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
