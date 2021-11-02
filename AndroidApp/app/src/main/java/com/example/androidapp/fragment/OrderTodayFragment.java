6e0a7b0af8ad33865f979e6a42bd85bdc35e0054
Anhduy-git <anhduydp123@gmail.com>
2021-11-02 19:43:32 +0700

    Update Order Activity

--- AndroidApp/app/src/main/java/com/example/androidapp/fragment/OrderTodayFragment.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/fragment/OrderTodayFragment.java
@@ -1,20 +1,39 @@
 package com.example.androidapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.androidapp.MainActivity;
+import com.example.androidapp.NewOrderActivity;
 import com.example.androidapp.R;
 
 public class OrderTodayFragment extends Fragment {
+    public Button btnAddNewOrder;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_order_today, container, false);
+
+        //Button to launch Add New Order Activity
+        View view = inflater.inflate(R.layout.fragment_order_today,
+                container, false);
+        btnAddNewOrder = (Button)view.findViewById(R.id.add_new_order);
+        btnAddNewOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NewOrderActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
+
 }
