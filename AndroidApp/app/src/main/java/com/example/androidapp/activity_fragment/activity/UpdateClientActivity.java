8472543fd538558a1f5e006a0d1793349caeebd9
PDAQ1906 <20120635@student.hcmus.edu.vn>
2021-11-08 01:01:16 +0700

    Clients have done

--- AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/UpdateClientActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/UpdateClientActivity.java
@@ -1,5 +1,4 @@
 package com.example.androidapp.activity_fragment.activity;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,9 +10,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.androidapp.R;
-
-public class NewClientActivity extends AppCompatActivity {
-
+public class UpdateClientActivity extends AppCompatActivity {
+    public static final String EXTRA_CLIENT_ID =
+            "com.example.androidapp.EXTRA_CLIENT_ID";
     public static final String EXTRA_CLIENT_NAME =
             "com.example.androidapp.EXTRA_CLIENT_NAME";
     public static final String EXTRA_CLIENT_NUMBER =
@@ -24,20 +23,26 @@ public class NewClientActivity extends AppCompatActivity {
     private EditText editClientName;
     private EditText editClientNumber;
     private EditText editClientAddress;
-    private Button btnAddClient;
+    private Button btnUpdateClient;
     private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_client);
+        setContentView(R.layout.activity_confirm_client);
 
         initUi();
 
-        btnAddClient.setOnClickListener(new View.OnClickListener() {
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_CLIENT_ID)) {
+            editClientName.setText(intent.getStringExtra(EXTRA_CLIENT_NAME));
+            editClientNumber.setText(intent.getStringExtra(EXTRA_CLIENT_NUMBER));
+            editClientAddress.setText(intent.getStringExtra(EXTRA_CLIENT_ADDRESS));
+        }
+        btnUpdateClient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addClient();
+                updateClient();
             }
         });
 
@@ -49,13 +54,13 @@ public class NewClientActivity extends AppCompatActivity {
         });
     }
     private void initUi () {
-        editClientName = findViewById(R.id.add_client_name);
-        editClientNumber = findViewById(R.id.add_client_number);
-        editClientAddress = findViewById(R.id.add_client_address);
-        btnAddClient = findViewById(R.id.add_client_info_button);
-        btnBack = findViewById(R.id.new_client_back_btn);
+        editClientName = findViewById(R.id.update_client_name);
+        editClientNumber = findViewById(R.id.update_client_number);
+        editClientAddress = findViewById(R.id.update_client_address);
+        btnUpdateClient = findViewById(R.id.update_client_info_button);
+        btnBack = findViewById(R.id.confirm_client_back_btn);
     }
-    private void addClient() {
+    private void updateClient() {
         String strClientName = editClientName.getText().toString().trim();
         String strClientNumber = editClientNumber.getText().toString().trim();
         String strClientAddress = editClientAddress.getText().toString().trim();
@@ -71,8 +76,11 @@ public class NewClientActivity extends AppCompatActivity {
         data.putExtra(EXTRA_CLIENT_NUMBER, strClientNumber);
         data.putExtra(EXTRA_CLIENT_ADDRESS, strClientAddress);
 
+        int id = getIntent().getIntExtra(EXTRA_CLIENT_ID, -1);
+        if (id != -1) {
+            data.putExtra(EXTRA_CLIENT_ID, id);
+        }
         setResult(RESULT_OK, data);
         finish();
     }
 }
-
