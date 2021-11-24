2f89322f761a5f6b795d664aa14880fb48135630
Anhduy-git <anhduydp123@gmail.com>
2021-11-24 17:58:51 +0700

    add notify

--- AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.androidapp.activity_fragment.activity;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Button;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -37,13 +42,13 @@ import org.joda.time.DateTimeComparator;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+
 import java.util.Calendar;
 
 import java.util.Date;
 import java.util.List;
 
-import java.util.TimeZone;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -61,101 +66,58 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private OrderViewModel orderViewModel;
     private UnpaidOrderViewModel unpaidOrderViewModel;
     private UpcomingOrderViewModel upcomingOrderViewModel;
-
+    private SimpleDateFormat simpleDateFormat;
+    private DateTimeComparator dateTimeComparator;
+    private Date today;
+    private Date tomorrow;
+    private Calendar calendarNotification;
+    private Calendar calendarToday;
+    private Calendar calendarTomorrow;
+    private int numTomorrowOrder;
+    public static final String CHANNEL_ID = "CHANNEL 1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Order Today");
-
         mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView nav_view = findViewById(R.id.nav_view);
         nav_view.setNavigationItemSelectedListener(this);
         replaceFragment(new OrderTodayFragment());
-
-
+        //select the current fragment from intent
+        selectFragment();
 
         //Setup View Model
         orderViewModel = new ViewModelProvider(this).get(OrderViewModel.class);
         unpaidOrderViewModel = new ViewModelProvider(this).get(UnpaidOrderViewModel.class);
         upcomingOrderViewModel = new ViewModelProvider(this).get(UpcomingOrderViewModel.class);
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+7:00"));
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        //Date format
+        simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         //Only compare the date
-        DateTimeComparator dateTimeComparator = DateTimeComparator.getDateOnlyInstance();
-        //Get the current date
-        Date today  = calendar.getTime();
+        dateTimeComparator = DateTimeComparator.getDateOnlyInstance();
+        //Get today's date
+        calendarToday = Calendar.getInstance();
+        today = calendarToday.getTime();
+        //Get tomorrow's date
+        calendarTomorrow = Calendar.getInstance();
+        calendarTomorrow.add(Calendar.DAY_OF_YEAR, 1);
+        tomorrow = calendarTomorrow.getTime();
 
-        //Update item from Today Order to Unpaid Order and History
-        orderViewModel.getAllOrder().observe(MainActivity.this, new Observer<List<Order>>() {
-            @Override
-            public void onChanged(List<Order> orders) {
-                for (Order order : orders) {
-                    try {
-                        Date orderDate = simpleDateFormat.parse(order.getDate());
-                        int ret = dateTimeComparator.compare(orderDate, today);
-                        if (ret < 0) {
-                            //if shipped
-                            if (order.getShip()) {
-                                //if paid then move only to history success order
-                                if (order.getPaid()) {
-                                    //
-                                } else { // move to history success and unpaid order.
-                                    Client client = new Client(order.getClient().getClientName(), order.getClient().getPhoneNumber(),
-                                            order.getClient().getAddress());
-                                    UnpaidOrder unpaidOrder = new UnpaidOrder(client, order.getDate(), order.getTime(), order.getPrice(), false, order.getOrderListDish());
-                                    unpaidOrderViewModel.insert(unpaidOrder);
-                                }
-                            } else {
-                                //Move to history cancel
-                            }
-                            //Remove all old order
-                            orderViewModel.delete(order);
-                        }
-                    } catch (ParseException ex) {
-
-                    }
-                }
-            }
-        });
+        //Update the number of tomorrow order and notify
+        getNumTomorrowOrderAndNotify();
 
         //Update Upcoming Order to Order Today
-        upcomingOrderViewModel.getAllUpcomingOrder().observe(MainActivity.this, new Observer<List<UpcomingOrder>>() {
-            @Override
-            public void onChanged(List<UpcomingOrder> upcomingOrders) {
-                for (UpcomingOrder upcomingOrder : upcomingOrders) {
-                    //Get the day of upcomingOrder
-                    try {
-                        Date upcomingOrderDay = simpleDateFormat.parse(upcomingOrder.getDate());
-                        //Check if the upcomingOrderDay is today
-                        int ret = dateTimeComparator.compare(upcomingOrderDay, today);
-                        if (ret == 0) {
-                            Client client = new Client(upcomingOrder.getClient().getClientName(), upcomingOrder.getClient().getPhoneNumber(),
-                                    upcomingOrder.getClient().getAddress());
-                            Order order = new Order(client, upcomingOrder.getDate(), upcomingOrder.getTime(),
-                                    upcomingOrder.getPrice(), false, upcomingOrder.getPaid(), upcomingOrder.getOrderListDish());
-                            //add upcomingOrder to today's Order
-                            orderViewModel.insert(order);
-                            //remove that upcomingOrder
-                            upcomingOrderViewModel.delete(upcomingOrder);
-                        }
+        updateUpcomingOrder();
 
-                    } catch (ParseException ex) {
+        //Update item from Today Order to Unpaid Order and History
+        updateUnpaidOrderAndHistory();
 
-                    }
-
-
-                }
-            }
-        });
 
     }
 
@@ -245,10 +207,138 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
+    //Select the current fragment from intent
+    private void  selectFragment() {
+        int intentFragment = getIntent().getIntExtra("fragmentSelect", -1);
+
+        switch (intentFragment){
+            case FRAGMENT_UPCOMING_ORDER:
+                replaceFragment(new UpcomingOrderFragment());
+                getSupportActionBar().setTitle("Upcoming Order");
+                mCurrentFragment = FRAGMENT_UPCOMING_ORDER;
+                break;
+        }
+    }
 
     private void replaceFragment (Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.content_frame, fragment);
         transaction.commit();
+    }
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "NotificationChannel";
+            String description = "Channel for notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+
+        }
+    }
+    private void updateUpcomingOrder() {
+        upcomingOrderViewModel.getAllUpcomingOrder().observe(MainActivity.this, new Observer<List<UpcomingOrder>>() {
+            @Override
+            public void onChanged(List<UpcomingOrder> upcomingOrders) {
+                for (UpcomingOrder upcomingOrder : upcomingOrders) {
+                    //Get the day of upcomingOrder
+                    try {
+                        Date upcomingOrderDate = simpleDateFormat.parse(upcomingOrder.getDate());
+                        //Check if the upcomingOrderDay is today
+                        int ret = dateTimeComparator.compare(upcomingOrderDate, today);
+                        if (ret == 0) {
+                            Client client = new Client(upcomingOrder.getClient().getClientName(), upcomingOrder.getClient().getPhoneNumber(),
+                                    upcomingOrder.getClient().getAddress());
+                            Order order = new Order(client, upcomingOrder.getDate(), upcomingOrder.getTime(),
+                                    upcomingOrder.getPrice(), false, upcomingOrder.getPaid(), upcomingOrder.getOrderListDish());
+                            //add upcomingOrder to today's Order
+                            orderViewModel.insert(order);
+                            //remove that upcomingOrder
+                            upcomingOrderViewModel.delete(upcomingOrder);
+                        }
+
+                    } catch (ParseException ex) {
+
+                    }
+                }
+            }
+        });
+    }
+    private void updateUnpaidOrderAndHistory() {
+        orderViewModel.getAllOrder().observe(MainActivity.this, new Observer<List<Order>>() {
+            @Override
+            public void onChanged(List<Order> orders) {
+                for (Order order : orders) {
+                    try {
+                        Date orderDate = simpleDateFormat.parse(order.getDate());
+                        int ret = dateTimeComparator.compare(orderDate, today);
+                        if (ret < 0) {
+                            //if shipped
+                            if (order.getShip()) {
+                                //if paid then move only to history success order
+                                if (order.getPaid()) {
+                                    //
+                                } else { // move to history success and unpaid order.
+                                    Client client = new Client(order.getClient().getClientName(), order.getClient().getPhoneNumber(),
+                                            order.getClient().getAddress());
+                                    UnpaidOrder unpaidOrder = new UnpaidOrder(client, order.getDate(), order.getTime(), order.getPrice(), false, order.getOrderListDish());
+                                    unpaidOrderViewModel.insert(unpaidOrder);
+                                }
+                            } else {
+                                //Move to history cancel
+                            }
+                            //Remove all old order
+                            orderViewModel.delete(order);
+                        }
+                    } catch (ParseException ex) {
+
+                    }
+                }
+            }
+        });
+    }
+    private void getNumTomorrowOrderAndNotify() {
+        upcomingOrderViewModel.getAllUpcomingOrder().observe(MainActivity.this, new Observer<List<UpcomingOrder>>() {
+            @Override
+            public void onChanged(List<UpcomingOrder> upcomingOrders) {
+                numTomorrowOrder = 0;
+                for (UpcomingOrder upcomingOrder : upcomingOrders) {
+                    //Get the day of upcomingOrder
+                    try {
+                        Date upcomingOrderDate = simpleDateFormat.parse(upcomingOrder.getDate());
+                        //Check if the upcomingOrderDay is today
+                        int ret = dateTimeComparator.compare(upcomingOrderDate, tomorrow);
+                        if (ret == 0) {
+                            numTomorrowOrder += 1;
+
+                        }
+
+                    } catch (ParseException ex) {
+
+                    }
+                }
+                setNotification();
+            }
+        });
+    }
+    private void setNotification() {
+        //Notification
+        createNotificationChannel();
+        //set time daily for notification
+        calendarNotification = Calendar.getInstance();
+        calendarNotification.set(Calendar.HOUR_OF_DAY, 19);
+        calendarNotification.set(Calendar.MINUTE, 0);
+        calendarNotification.set(Calendar.SECOND, 0);
+
+        Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
+
+        intent.putExtra("numOrderTomorrow", numTomorrowOrder);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendarNotification.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
     }
 }
