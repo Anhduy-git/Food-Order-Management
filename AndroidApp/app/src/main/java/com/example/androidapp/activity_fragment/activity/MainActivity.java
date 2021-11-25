d9b04c7a22e693dde5b6cd161d1d61613786f2c6
Anhduy-git <anhduydp123@gmail.com>
2021-11-25 09:35:53 +0700

    update notify

--- AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/MainActivity.java
@@ -329,8 +329,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //set time daily for notification
         calendarNotification = Calendar.getInstance();
-        calendarNotification.set(Calendar.HOUR_OF_DAY, 9);
-        calendarNotification.set(Calendar.MINUTE, 31);
+        calendarNotification.set(Calendar.HOUR_OF_DAY, 20);
+        calendarNotification.set(Calendar.MINUTE, 0);
         calendarNotification.set(Calendar.SECOND, 0);
         //set notify only 1 time in day
         if (Calendar.getInstance().after(calendarNotification)) {
