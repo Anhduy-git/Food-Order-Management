efc1d2933b294a64efc47b141080cf4097d5621d
0naha0 <namhoailu@gmail.com>
2021-11-29 17:37:53 +0700

    add supplement his info

---
+++ AndroidApp/app/src/main/java/com/example/androidapp/activity_fragment/activity/HistoryInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidapp.activity_fragment.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.androidapp.R;
+import android.os.Bundle;
+
+public class HistoryInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_info);
+    }
+}
